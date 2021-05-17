4790a1289ed6bc5973584134f43a24861862efa1
Timothy Kwon <timothysoongil@gmail.com>
2021-05-17 16:22:27 -0700

    main menu + bats

---
+++ Assets/Scripts/Bat.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bat : MonoBehaviour
+{
+    public AnimationCurve bounce;
+    public float walkDist = 5;
+    private Vector3 changedPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        changedPos = transform.position;
+        changedPos.x += (Mathf.PingPong(Time.time, walkDist) - (walkDist / 2)) * .01f;
+        changedPos.y += Mathf.Lerp(0, bounce.Evaluate(Time.time), Time.time) * .01f; ;
+
+        transform.position = changedPos;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.tag == "bubble")
+        {
+            Destroy(gameObject);
+        }
+
+        if (collider.gameObject.tag == "Player")
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene("FirstScene");
+        }
+    }
+}
